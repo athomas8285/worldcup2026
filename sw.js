a1504f920edd2d46bafab8ff5fdd72ebe5a29054
gjj <gjj@localhost>
2026-06-15 22:45:02 +0800

    init: V3.3.3 World Cup 2026 app

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'v333-v1';
+const URLS = ['merged.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
